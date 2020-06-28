c3b656a48c8a88147d2cb7a5b0de09b9dfba0838
ahmodadeola <ahmodadeola@gmail.com>
2020-06-28 20:39:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let Item;
+
+mongoose
+  .connect("mongodb://localhost:27017/itemStore", {
+    useNewUrlParser: true,
+  })
+  .then((connection) => {
+    // let bookSchema = mongoose.Schema({
+    //   title: String,
+    //   author: String,
+    //   ISO: Number,
+    //   date: Date,
+    // });
+
+    let itemSchema = mongoose.Schema({
+      img: String,
+      name: String,
+      brand: String,
+      description: String,
+      serialNo: Number,
+      costPrice: Number,
+      sellPrice: Number,
+      quantity: Number,
+      category: String,
+      date: Date,
+    });
+
+    // let Book = mongoose.model("Book", bookSchema);
+    Item = mongoose.model("Item", itemSchema);
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Its all fine");
+});
+
+app.post("/api/additem", (req, res) => {
+  let item = new Item({ ...req.body, date: new Date() });
+  item
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
+});
+
+app.get("/api/getitems", (req, res) => {
+  Item.find().then((data) => res.json({ dataCount: data.length, data: data }));
+});
+
+app.listen(8080, () => console.log("server started at http://localhost:8080"));
