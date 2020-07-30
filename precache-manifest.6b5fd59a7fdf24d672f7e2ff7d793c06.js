282a0c13e7d2e4100739ce826bb828ef93976efe
ahmodadeola <ahmodadeola@gmail.com>
2020-07-30 01:06:35 +0100

    Updates

---
+++ precache-manifest.6b5fd59a7fdf24d672f7e2ff7d793c06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417cbf24441fe1f330d00ab15cb07bf3",
+    "url": "/update_X/index.html"
+  },
+  {
+    "revision": "812fe3e37e6acbdb2bed",
+    "url": "/update_X/static/css/main.f0f7e5ef.chunk.css"
+  },
+  {
+    "revision": "e676530a2325bd209f35",
+    "url": "/update_X/static/js/2.dc1efa28.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/update_X/static/js/2.dc1efa28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812fe3e37e6acbdb2bed",
+    "url": "/update_X/static/js/main.2dfe018f.chunk.js"
+  },
+  {
+    "revision": "f49edc8619c18f8ba89a",
+    "url": "/update_X/static/js/runtime-main.5bb32177.js"
+  },
+  {
+    "revision": "38979742c9490ef3aad48776b1f27ecd",
+    "url": "/update_X/static/media/foods.38979742.jpg"
+  },
+  {
+    "revision": "a155e3af38b5d892ca31743c5f631533",
+    "url": "/update_X/static/media/game_console.a155e3af.jpg"
+  }
+]);
